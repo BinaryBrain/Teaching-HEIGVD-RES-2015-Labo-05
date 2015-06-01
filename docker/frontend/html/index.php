3c0992bf5c7066a22528060861d22db0e38b08cd
BafS <fabacrans@hotmail.com>
2015-06-01 16:24:06 +0200

    pretty print + cookie info

--- docker/frontend/html/index.php
+++ docker/frontend/html/index.php
@@ -5,4 +5,8 @@ session_start();
 $serverIp = $_SERVER['SERVER_ADDR'];
 $VMIp = $_SERVER['SERVER_NAME']; // HTTP_HOST with port
 
+function prettyPrint(array $arr) {
+	return json_encode($arr, JSON_PRETTY_PRINT);
+}
+
 include_once 'view.html';
