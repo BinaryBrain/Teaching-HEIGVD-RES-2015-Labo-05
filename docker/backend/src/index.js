6d12c1cd9c33e376cf3895b208c32bc51e0fe32c
BafS <fabacrans@hotmail.com>
2015-06-01 23:15:45 +0200

    add CORS

--- docker/backend/src/index.js
+++ docker/backend/src/index.js
@@ -3,6 +3,15 @@ var os = require('os');
 var app = express();
 
 /**
+ * CORS shit
+ */
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+});
+
+/**
  * Random number between 0 and 10000
  */
 app.get('/rand', function(req, res) {
