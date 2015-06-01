c29d33c63b4b8b3a1dbe8fa87631b55480584392
BafS <fabacrans@hotmail.com>
2015-06-02 00:13:39 +0200

    run ok

--- docker/loadbalancer/src/run.sh
+++ docker/loadbalancer/src/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# pm2 start -x /src/heartbeathandler.js --no-daemon
-node /src/heartbeathandler.js -DFOREGROUND
-# pm2 monit
-# pm2 start /src/heartbeathandler.js
+apachectl start
+
+pm2 start -x /src/heartbeathandler.js --no-daemon
+pm2 monit
