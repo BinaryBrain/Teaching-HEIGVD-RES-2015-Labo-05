d1a12951652316d0e9c046b43abd5840cb66f7ae
BafS <fabacrans@hotmail.com>
2015-06-01 03:20:04 +0200

    add second frontend

--- labo_halt.sh
+++ labo_halt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-containers="res-frontend-run1 res-backend-run1"
+containers="res-frontend-run1 res-frontend-run2 res-backend-run1 res-loadbalancer-run1"
 
 echo "Stop containers"
 docker stop $containers
