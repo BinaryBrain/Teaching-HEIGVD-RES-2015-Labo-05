098dc8c83373b239e6c8e10788378e3452c0e3ac
BafS <fabacrans@hotmail.com>
2015-06-01 21:44:16 +0200

    clean script

--- labo_run.sh
+++ labo_run.sh
@@ -4,34 +4,28 @@ bold=$(tput bold)
 normal=$(tput sgr0)
 green=$(tput setaf 2)
 
+# Run docker
+function run {
+	echo "${bold}$1: "
+	docker run -d --name "$1-$2" -p $3:$4 $1
+	containerIp "$1-$2"
+}
+
+# Docker ip
 function containerIp {
-	echo "${bold}IP $1: ${green}"
+	echo "IP $1: ${green}"
 	docker inspect --format '{{ .NetworkSettings.IPAddress }}' $1
 	echo "${normal}"
 }
 
-function run {
-	docker run -d --name "$1-$2" -p $3:$4 $1
-}
-
 
 # Frontend
-# docker run -it -d --name res-frontend1 res-run-frontend
-docker run -d --name res-frontend-run1 -p 80:80 res-frontend
-# docker run -d --name res-frontend-run1 -p 80:8011 res-frontend
-docker run -d --name res-frontend-run2 -p 9999:80 res-frontend
-
+# docker run -d --name res-frontend-run1 -p 80:80 res-frontend
+run res-frontend run1 80 80
+run res-frontend run2 9999 80
 
 # Backend
-# docker run -it -d --name res-backend1 res-run-backend
-docker run -d --name res-backend-run1 -p 3000:3000 res-backend
-
-
-# Containers IP
-containerIp res-frontend-run1
-containerIp res-frontend-run2
-containerIp res-backend-run1
-
+run res-backend run1 3000 3000
 
 
 # docker run res-backend node index.js
