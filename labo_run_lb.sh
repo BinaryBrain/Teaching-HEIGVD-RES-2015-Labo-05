96ef863c02196b2914f45d05228f1cbb7ab2fad6
BafS <fabacrans@hotmail.com>
2015-06-01 03:34:23 +0200

    use port 8080

--- labo_run_lb.sh
+++ labo_run_lb.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-docker run -it -d --name res-loadbalancer-run1 -p 80:80 res-loadbalancer
+docker run -it -d --name res-loadbalancer-run1 -p 8080:8080 res-loadbalancer
+# docker run -it -d --name res-loadbalancer-run1 -p 80:80 res-loadbalancer
+
+docker inspect --format '{{ .NetworkSettings.IPAddress }}' res-loadbalancer-run1
